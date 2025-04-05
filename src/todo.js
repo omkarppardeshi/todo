8e347ae2716d377adb975570bc5c85a1dc57c04b
Omkar Pardeshi <omkar.17.pardeshi@gmail.com>
2025-04-05 20:24:14 +0530

    CSS Changes

--- src/todo.js
+++ src/todo.js
@@ -11,11 +11,20 @@ import {
   Form,
   FormGroup,
   Table,
+  Container,
+  Navbar,
+  NavbarText,
+  NavbarBrand,
+  Alert,
+  CardTitle,
+  FormFeedback,
 } from "reactstrap";
 
 function Todo() {
   const [list, setList] = useState([]);
   const [task, setTask] = useState("");
+  const [message, setmessage] = useState(false);
+  console.log("Task", list);
 
   const addTask = () => {
     if (task.trim() !== "") {
@@ -27,72 +36,123 @@ function Todo() {
   const onSubmit = (e) => {
     e.preventDefault();
     addTask();
+    setmessage(false);
+  };
+
+  const handleRemove = (item) => {
+    console.log("Res", item);
+    const updatedList = list.filter((items, i) => i !== item);
+    setList(updatedList);
+    setmessage(true);
   };
 
   return (
-    <Row className="justify-content-center mt-4">
-      {/* To-Do List Table */}
-      {list.length > 0 && (
-        <Col md="6">
-        <Card className="shadow">
-          <CardHeader>
-            <h3 className="text-center">To-Do List</h3>
-          </CardHeader>
-          <CardBody>
-            <Table bordered responsive>
-              <thead className="bg-light">
-                <tr>
-                  <th>Sr no.</th>
-                  <th>Task</th>
-                </tr>
-              </thead>
-              <tbody>
-                {list.length === 0 ? (
-                  <tr>
-                    <td colSpan="2" className="text-center">
-                      No tasks added yet.
-                    </td>
-                  </tr>
-                ) : (
-                  list.map((item, index) => (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>{item}</td>
+    <Container>
+      <Row className="justify-content-center mt-4">
+        <Col>
+          {/* <Alert color="primary">Task removed!</Alert> */}
+
+          {/* Add Task Form */}
+
+          <Card className="shadow p-3 mb-3">
+            <Navbar className="mx-auto">
+              <NavbarBrand>
+                <img src={require("./images/tutedude.png")} />
+                <h2>To-Do List</h2>
+              </NavbarBrand>
+            </Navbar>
+            <CardBody>
+              <Form onSubmit={onSubmit} className="d-flex flex-column">
+                <Row>
+                  <Col md={10}>
+                    <FormGroup>
+                      <Input
+                        id="taskname"
+                        type="text"
+                        placeholder="Enter a task name"
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)}
+                        required
+                      />
+                    </FormGroup>
+                  </Col>
+
+                  <Col md={2}>
+                    <Button type="submit" color="primary">
+                      Add Task
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            {message && (
+              <small className="text-danger">Task Removed Successfully!</small>
+            )}
+            </CardBody>
+          </Card>
+
+          {/* {list.length > 0 && (
+            <Card className="shadow">
+              <CardBody>
+                <Table bordered responsive>
+                  <thead className="bg-light">
+                    <tr>
+                      <th>Sr no.</th>
+                      <th>Task</th>
+                      <th>Action</th>
                     </tr>
-                  ))
-                )}
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
-      </Col>
-
-      )}
-      
-
-      {/* Add Task Form */}
-      <Col md="4">
-        <Card className="shadow p-3">
-          <CardBody>
-            <Form onSubmit={onSubmit} className="d-flex flex-column align-items-center">
-              <FormGroup className="w-100">
-                <Label for="taskname">Task name:</Label>
-                <Input
-                  id="taskname"
-                  type="text"
-                  value={task}
-                  onChange={(e) => setTask(e.target.value)}
-                  required
-                />
-              </FormGroup>
-              <Button type="submit" color="primary" className="w-100">
-                Add Task
-              </Button>
-            </Form>
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
+                  </thead>
+                  <tbody>
+                    {list.length === 0 ? (
+                      <tr>
+                        <td colSpan="2" className="text-center">
+                          No tasks added yet.
+                        </td>
+                      </tr>
+                    ) : (
+                      list.map((item, index) => (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td>{item}</td>
+                          <td>
+                            <Button
+                              color="danger"
+                              onClick={() => handleRemove(index)}
+                            >
+                              Remove task
+                            </Button>
+                          </td>
+                        </tr>
+                      ))
+                    )}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+                      )} */}
+          <Container>
+            {list.map((item, index) => (
+              <Col md={12}>
+                <Card className="shadow mb-3" key={index}>
+                  <CardHeader>
+                    <div className="d-flex justify-content-between">
+                      <h3>{item}</h3>
+                      <Button
+                        color="danger"
+                        outline
+                        size="sm"
+                        onClick={() => handleRemove(index)}
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  </CardHeader>
+                </Card>
+              </Col>
+            ))}
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
